7723f88ee57b2ad3bb07f9c9e27fd7aa2f630e86
cristopher83 <111202708+cristopher83@users.noreply.github.com>
2022-11-11 14:47:54 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class Main{
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
 
         prueba1();
 
@@ -14,34 +14,55 @@ public class Main{
 
     }
 
-    private static void prueba1() throws IOException {
+    private static void prueba1() {
         File file = new File("prueba1.txt");
 
-        FileWriter fileWriter = new FileWriter(file);
+        try {
+            FileWriter fileWriter = new FileWriter(file);
+            fileWriter.write("En esta prueba el archivo se crea cada vez que el código se ejecuta");
+            fileWriter.close();
+        }catch (IOException ex){
+            ex.printStackTrace();
+        }
 
-        fileWriter.write("En esta prueba el archivo se crea cada vez que el código se ejecuta");
-
-        fileWriter.close();
     }
 
-    private static void prueba2() throws IOException{
+    private static void prueba2(){
         File file = new File("prueba2.txt");
-
+    try {
         FileWriter fileWriter = new FileWriter(file, true);
-
         fileWriter.write("En esta prueba el archivo crece en cada ejecución, lo nuevo se añade al final\n");
-
         fileWriter.close();
+    }catch (IOException ex){
+        ex.printStackTrace();
     }
 
-    private static void prueba3() throws IOException{
+    }
+
+    private static void prueba3(){
         File file = new File("prueba3.txt");
-        FileWriter fileWriter = new FileWriter(file);
+        FileWriter fileWriter = null;
+        try {
+            fileWriter = new FileWriter(file);
 
-        fileWriter.write("En esta prueba ocurrirá un error\n");
-        //fileWriter.write("El resultado fallará: " + (1 / 0));
+            fileWriter.write("En esta prueba ocurrirá un error\n");
+            fileWriter.write("El resultado fallará: " + (1 / 0));
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }catch (ArithmeticException e){
+            throw new RuntimeException("re");
+        }
+        finally {
+            if(fileWriter != null){
+                try{
+                    fileWriter.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
 
-        fileWriter.close();
+
 
     }
 
